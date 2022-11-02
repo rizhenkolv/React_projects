b8d5bd40889fcc9e5a3e10f2aaf9a8faf61e0e10
rizhenkolv <rizhenkolv@gmail.com>
2022-11-02 20:17:37 +0100

    current

--- grocery_bud/src/App.js
+++ grocery_bud/src/App.js
@@ -2,9 +2,18 @@ import React, { useState, useEffect } from 'react'
 import List from './List'
 import Alert from './Alert'
 
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list')
+  if (list) {
+    return JSON.parse(localStorage.getItem('list'))
+  } else {
+    return []
+  }
+}
+
 const App = () => {
   const [name, setName] = useState('')
-  const [list, setList] = useState([])
+  const [list, setList] = useState(getLocalStorage())
   const [isEditing, setIsEditing] = useState(false)
   const [editID, setEditID] = useState(null)
   const [alert, setAlert] = useState({
@@ -19,8 +28,21 @@ const App = () => {
       showAlert(true, 'danger', 'please enter value')
     } else if (name && isEditing) {
       //deal with edit
+      setList(
+        list.map((item) => {
+          if (item.id === editID) {
+            return { ...item, title: name }
+          }
+          return item
+        })
+      )
+      setName('')
+      setEditID(null)
+      setIsEditing(false)
+      showAlert(true, 'success', 'value changed')
     } else {
       //show alert
+      showAlert(true, 'success', 'item added to the list')
       const newItem = { id: new Date().getTime().toString(), title: name }
       setList([...list, newItem])
       setName('')
@@ -29,6 +51,23 @@ const App = () => {
   const showAlert = (show = false, type = '', msg = '') => {
     setAlert({ show, type, msg })
   }
+  const clearList = () => {
+    showAlert(true, 'danger', 'empty list')
+    setList([])
+  }
+  const removeItem = (id) => {
+    showAlert(true, 'danger', 'item removed')
+    setList(list.filter((item) => item.id !== id))
+  }
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id)
+    setIsEditing(true)
+    setEditID(id)
+    setName(specificItem.title)
+  }
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list))
+  })
   return (
     <section className='section-center'>
       <form className='grocery-form' onSubmit={handleSubmit}>
@@ -48,8 +87,15 @@ const App = () => {
       </form>
       {list.length > 0 && (
         <div className='grocery-container'>
-          <List items={list} />
-          <button className='clear-btn'>Clear Items</button>
+          <List
+            items={list}
+            removeItem={removeItem}
+            editItem={editItem}
+            list={list}
+          />
+          <button className='clear-btn' onClick={clearList}>
+            Clear Items
+          </button>
         </div>
       )}
     </section>
